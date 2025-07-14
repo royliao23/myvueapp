d530953a7750143fe0293350388ae4de06a72d7f
Roy Y Liao <yunzhi.liao@gmail.com>
2025-07-14 10:41:53 +1000

    pay view with email function

--- src/pages/PayView.vue
+++ src/pages/PayView.vue
@@ -237,7 +237,9 @@ const handlePrint = () => {
   }, 200);
 };
 
-
+const emailJsKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+const emailJsServiceId = import.meta.env.VITE_SERVICE_ID;
+const emailJsTemplateId = import.meta.env.VITE_OTID;
 const handleEmail = async () => {
   try {
     const templateParams = {
@@ -251,14 +253,13 @@ const handleEmail = async () => {
       project_name: projectDetails.value.project_name,
       to_email: "yunzhi.liao@me.com" // contractorDetails.value.email,
     };
-
     await emailjs.send(
-      import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      emailJsServiceId as string,
+      emailJsTemplateId as string,
       templateParams,
-      import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      emailJsKey as string
     );
-
+ 
     alert('Email sent successfully!');
   } catch (error) {
     console.error('Failed to send email:', error);
